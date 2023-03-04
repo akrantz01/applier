095277531bf08ae761a33b874deea40d473eeec2
Alex Krantz <alex@krantz.dev>
2023-03-04 15:26:22 -0800

    fix: properly template default branch

--- applier/app.py
+++ applier/app.py
@@ -1,5 +1,3 @@
-from os import environ
-
 from flask import Flask, jsonify, request
 from werkzeug.middleware.proxy_fix import ProxyFix
 
@@ -14,7 +12,7 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 @app.post("/webhook/apply")
 @webhook
 def apply():
-    if request.json.get("ref") != "refs/heads/{DEFAULT_BRANCH}":
+    if request.json.get("ref") != f"refs/heads/{DEFAULT_BRANCH}":
         return jsonify(message="not main branch, skipping")
 
     sync()
